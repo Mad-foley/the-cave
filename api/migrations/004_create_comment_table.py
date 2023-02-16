4ba4f6b20b6ee5167a4e30675790e603d601b83c
Kyle <kylemerckx@gmail.com>
2023-02-15 18:05:40 -0600

    finished the routes and queries for comments

--- api/migrations/004_create_comment_table.py
+++ api/migrations/004_create_comment_table.py
@@ -4,8 +4,8 @@ steps = [
         """
         CREATE TABLE comments (
             id SERIAL PRIMARY KEY NOT NULL,
-            wine_id INTEGER NOT NULL,
-            user_id INTEGER NOT NULL,
+            wine_id INTEGER NOT NULL REFERENCES wines(id) ON DELETE CASCADE,
+            user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
             created_on TIMESTAMP,
             comment VARCHAR(1000) NOT NULL
         );
