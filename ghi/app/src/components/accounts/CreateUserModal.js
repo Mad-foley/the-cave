1796746a67365d7746ef1812a82234cf00d546dc
Malcolm Pei <pei.malcolm@gmail.com>
2023-02-22 19:06:40 -0500

    tailwind start

--- ghi/app/src/components/accounts/CreateUserModal.js
+++ ghi/app/src/components/accounts/CreateUserModal.js
@@ -18,16 +18,40 @@ export default function CreateUserForm() {
         e.preventDefault()
         createUser(formData)
     }
-
+    
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input onChange={handleFormChange} name="name" />
-                <input onChange={handleFormChange} name="username"/>
-                <input onChange={handleFormChange} name="password"/>
-                <input onChange={handleFormChange} type="date" name="birthday"/>
-                <input onChange={handleFormChange} name="image_url"/>
-                <button>Submit</button>
+        <div className="container mx-auto flex justify-center p-5 text-center">
+            <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <input
+                onChange={handleFormChange}
+                name="name"
+                placeholder='Full name'
+                className="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input
+                onChange={handleFormChange}
+                name="username"
+                placeholder='Username'
+                className="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input
+                onChange={handleFormChange}
+                name="password"
+                placeholder="Password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input
+                onChange={handleFormChange}
+                type="date"
+                name="birthday"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input
+                onChange={handleFormChange}
+                name="image_url"
+                placeholder="Profile picture URL"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                <button
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                >Submit</button>
             </form>
         </div>
     )
--- ghi/app/src/components/accounts/LogInModal.js
+++ ghi/app/src/components/accounts/LogInModal.js
@@ -23,11 +23,24 @@ export default function LogInForm() {
 
     }
     return (
-        <div className='container mx-auto shadow p-5'>
-            <form className='shadow p-5' onSubmit={handleSubmit}>
-                <input onChange={handleFormData} name="username" />
-                <input onChange={handleFormData} name="password" />
-                <button>test</button>
+        <div className='container mx-auto flex justify-content p-5 text-center'>
+            <form className='w-full max-w-xs shadow p-5' onSubmit={handleSubmit}>
+                <h1>Log In</h1>
+                <div className='mb-4 field'>
+                    <input
+                    onChange={handleFormData}
+                    name="username"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                </div>
+                <div>
+                    <input
+                    onChange={handleFormData}
+                    name="password"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                </div>
+                <button
+                className='inline-block align-basline font-bold text-sm text-blue-500 hover:text-blue-800'
+                >Log In</button>
             </form>
         </div>
     )
