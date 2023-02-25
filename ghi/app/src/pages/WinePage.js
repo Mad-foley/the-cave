b7fa20f2427228a9363c229343283b8511beb8e6
Malcolm Pei <pei.malcolm@gmail.com>
2023-02-24 20:29:18 -0500

    loading wines tweak idx

--- ghi/app/src/pages/WinePage.js
+++ ghi/app/src/pages/WinePage.js
@@ -1,25 +1,22 @@
-import {useEffect, useState} from 'react'
+import { useState} from 'react'
 import { useGetWinesQuery } from '../store/queries/wineApi'
 import WineCard from '../components/wines/WineCard'
 
-import { addWine } from '../store/queries/wineSlice'
-import { useDispatch } from 'react-redux'
-import { store } from '../store/store'
 
 function WinePage() {
     const {data: wines, isSuccess, isLoading} = useGetWinesQuery()
-    const [indexes, setIndexes] = useState({start:0,end:9})
+    const [indexes, setIndexes] = useState({start:-1,end:10})
 
     const handlePreviousPage = () => {
         setIndexes({start:indexes.start - 10, end:indexes.end - 10})
         if (indexes.start < 0) {
-            setIndexes({start:0, end:9})
+            setIndexes({start:-1, end:10})
         }
     }
     const handleNextPage = () => {
         setIndexes({start:indexes.start + 10, end:indexes.end + 10})
         if (indexes.start > wines.length) {
-            setIndexes({start:0, end:9})
+            setIndexes({start:-1, end:10})
         }
     }
 
