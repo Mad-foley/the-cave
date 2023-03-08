0f443a7ca8d04f4f5402a03451ee06a1be61f04d
Kyle <kylemerckx@gmail.com>
2023-03-08 16:18:26 -0600

    CORS

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ router = APIRouter()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
+        os.environ.get("CORS_HOST", 'PUBLIC_URL')
     ],
     allow_credentials=True,
     allow_methods=["*"],
