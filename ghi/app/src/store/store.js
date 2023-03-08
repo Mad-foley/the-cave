b7ba8e69980a5f414aeaa070b576bc2721982365
Kyle <kylemerckx@gmail.com>
2023-03-08 12:45:03 -0600

    work please

--- ghi/app/src/store/store.js
+++ ghi/app/src/store/store.js
@@ -18,8 +18,8 @@ export const store = configureStore({
         [ commentsApi.reducerPath ] : commentsApi.reducer,
         [ logsApi.reducerPath ] : logsApi.reducer,
         [ recApi.reducerPath ] : recApi.reducer,
-        ['wineRec']:wineRecSlice.reducer,
-        ['modalWindow']: modalSlice.reducer,
+        [wineRecSlice.reducerPath] : wineRecSlice.reducer,
+        [modalSlice.reducerPath] : modalSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
